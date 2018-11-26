f1c0b5b5d343dcb5317c77f15c64f6bf2adf2996
sondos alansi <sondos.alansi@yahoo.com>
2018-11-26 07:44:06 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,33 @@
 // Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+
+class SimpleComponent extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      mood: "happy"
+    }
+  }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    if (this.state.mood === "happy"){
+      this.setState({
+        mood: "sad"
+      },console.log(newMood))
+    } else {
+        this.setState({
+          mood: "happy"
+        },console.log(newMood))
+      }
+
+  }
+
+  render() {
+    return <div onClick={this.handleClick} >{this.state.mood}</div>;
+  }
+}
+
+export default SimpleComponent;
